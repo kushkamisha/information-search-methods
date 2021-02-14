5ca0293a92c32e350da551b629d57557b8bed206
Misha Kushka <kushka.misha@gmail.com>
2021-02-14 20:19:31 +0200

    btree

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-require('dotenv').config();
-const prompts = require('prompts');
-const { query } = require('./database');
+const { BTree } = require('./BTree');
 
-
-
-async function main() {
-  // const name = 'adolf deh';
-  // const limit = 3;
-
-  const { name, limit } = await prompts([{
-    type: 'text',
-    name: 'name',
-    message: 'Artist name?',
-  }, {
-    type: 'number',
-    name: 'limit',
-    message: 'Search limit?',
-    default: 5,
-  }]);
-
-  console.log({ name, limit });
-
-  const res = await query(`
-  SELECT * FROM artists
-  WHERE metaphone(name,6) % metaphone($1,6)
-  ORDER BY levenshtein(lower($1), lower(name))
-  limit $2;`, [name, limit]);
-  console.log(res);
-}
-
-main();
+const tree = new BTree(3);
+// 'Misha' 'Masha'
+tree.insert(5);
+// console.log(tree.searchValue('Misha'));
