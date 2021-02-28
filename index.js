72a31b790b1cd1e09e34b573d2cc6ee7eabaeea9
Misha Kushka <kushka.misha@gmail.com>
2021-02-28 15:23:19 +0200

    Clean up

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 const { JokerQueries } = require('./JokerQueries');
 const { PermutationIndex } = require('./PermutationIndex');
 const { ThreeGramIndex } = require('./ThreeGramIndex');
-const { GramTree } = require('./GramTree');
 
-const tree = new GramTree();
 const joker = new JokerQueries();
 const perms = new PermutationIndex();
 const tgram = new ThreeGramIndex();
 
-// joker.addWord('hi', 5);
-// joker.addWord('hey', 7);
-// joker.addWord('hello', 8);
-// joker.addWord('salo', 7);
-// joker.addWord('halo', 3);
+// Prefix + suffix trees
+joker.addWord('hi', 5);
+joker.addWord('hey', 7);
+joker.addWord('hello', 8);
+joker.addWord('salo', 7);
+joker.addWord('halo', 3);
 
-// console.log(joker.query('h*'));
-// console.log(joker.query('h*lo'));
-// console.log(joker.query('*lo'));
-// console.log(joker.query('x*'));
+console.log(joker.query('h*'));
+console.log(joker.query('h*lo'));
+console.log(joker.query('*lo'));
+console.log(joker.query('x*'));
 
-// perms.addWord('hi', 5);
-// perms.addWord('hey', 7);
-// perms.addWord('hello', 8);
-// perms.addWord('salo', 7);
-// perms.addWord('halo', 3);
+// Prefix tree with word permutaions
+perms.addWord('hi', 5);
+perms.addWord('hey', 7);
+perms.addWord('hello', 8);
+perms.addWord('salo', 7);
+perms.addWord('halo', 3);
 
-// console.log(perms.query('h*'));
-// console.log(perms.query('h*lo'));
-// console.log(perms.query('*lo'));
-// console.log(joker.query('x*'));
+console.log(perms.query('h*'));
+console.log(perms.query('h*lo'));
+console.log(perms.query('*lo'));
+console.log(joker.query('x*'));
 
-// console.log(tgram.__make3Grams('hello'));
-
+// 3-gram index
 tgram.addWord('hi', 5);
 tgram.addWord('hey', 7);
 tgram.addWord('hello', 8);
