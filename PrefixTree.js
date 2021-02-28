ad289e2f37a14806a73caaca04170228d7698627
Misha Kushka <kushka.misha@gmail.com>
2021-02-28 12:56:02 +0200

    PrefixTree: getSubtree by prefix

--- PrefixTree.js
+++ PrefixTree.js
@@ -6,14 +6,23 @@ class PrefixTree {
   addWord(word, docIndex, letters = this.letters) {
     const letter = word[0];
     if (!letters.has(letter)) {
-      letters.set(letter, { name: letter, docIndexes: new Set(), letters: new Map() });
+      letters.set(letter, { docIndexes: new Set(), letters: new Map() });
     }
     if (word.length === 1) {
       letters.get(letter).docIndexes.add(docIndex);
       return;
     }
     // console.log({ letters: letters.get(letter).letters });
-    this.addWord(word.slice(1), docIndex, letters.get(letter).letters);
+    return this.addWord(word.slice(1), docIndex, letters.get(letter).letters);
+  }
+
+  getSubtree(prefix, letters = this.letters) {
+    const letter = prefix[0];
+    if (letters.has(letter)) {
+      // console.log({ len: prefix.length, result: letters.get(letter) });
+      if (prefix.length === 1) return letters.get(letter);
+      return this.getSubtree(prefix.slice(1), letters.get(letter).letters);
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -4,4 +4,8 @@ const { PrefixTree } = require('./PrefixTree');
 
 const tree = new PrefixTree()
 tree.addWord('hi', 5);
-console.log(util.inspect(tree.letters, true, null, true));
+tree.addWord('hey', 7);
+tree.addWord('hello', 8);
+tree.addWord('yey', 7);
+console.log(util.inspect(tree.getSubtree('he'), true, null, true));
+// console.log(tree.getSubtree('he'));
