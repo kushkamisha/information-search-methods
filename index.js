4a778bfe579e00451a620856eb75a67286937447
Misha Kushka <kushka.misha@gmail.com>
2021-02-28 18:07:31 +0200

    First steps to read segment.txt by chunks

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
-const { read } = require('./utils');
+const fs = require('fs');
 const { stepMap, stepReduce } = require('./indexing');
+const { Liner } = require('./Liner');
 
 const filenames = [
   // "cut Война и мир. Том 1.txt",
   // "cut Бесы.txt",
-  // "Война и мир. Том 1.txt",
-  // "Война и мир. Том 2.txt",
-  // "Война и мир. Том 3.txt",
-  // "Война и мир. Том 4.txt",
-  // "Мастер и Маргарита.txt",
-  // "Волшебник Изумрудного города.txt",
-  // "Братья Карамазовы.txt",
-  // "Идиот.txt",
-  // "Униженные и оскорбленные.txt",
-  // "Бесы.txt",
+  "Война и мир. Том 1.txt",
+  "Война и мир. Том 2.txt",
+  "Война и мир. Том 3.txt",
+  "Война и мир. Том 4.txt",
+  "Мастер и Маргарита.txt",
+  "Волшебник Изумрудного города.txt",
+  "Братья Карамазовы.txt",
+  "Идиот.txt",
+  "Униженные и оскорбленные.txt",
+  "Бесы.txt",
 ];
 
 const main = async () => {
   const start = Date.now();
-  const data = await Promise.all(filenames.map(filename => read(filename)));
 
-  // await stepMap(data);
+  await stepMap(filenames);
   stepReduce();
 
   console.log(`Working time is ${Date.now() - start} ms`);
 }
 
-main();
+// main();
+
+// const source = fs.createReadStream('output/segment.txt');
+// const liner = new Liner({ delimiter: ',' });
+
+// source.pipe(liner);
+
+// liner.on('readable', () => {
+//   let line
+//   while (null !== (line = liner.read())) {
+//     console.log(line)
+//   }
+// })
+
+(async () => {
+  const file = 'output/segment.txt';
+  const stream = fs.createReadStream(file, { encoding: 'utf8', /*highWaterMark: 20000*/ });
+  stream.on('data', (chunk) => {
+    console.log(chunk.length);
+  })
+  // const firstByte = await stream[Symbol.asyncIterator]().next();
+  // for (firstByte of stream[Symbol.asyncIterator]().next()) {
+  //   console.log(`${file} >>> ${firstByte.value.length}`);
+  // }
+})()
--- indexing.js
+++ indexing.js
@@ -1,41 +1,53 @@
 const fs = require('fs');
 const path = require('path');
+const { read } = require('./utils');
 
-async function stepMap(data) {
-  return new Promise((resolve, reject) => {
-    try {
-      const segmentFile = fs.createWriteStream(path.join(__dirname, 'output', 'segment.txt'), {
-        flags: 'w',
-      });
-      let word = '';
+async function stepMap(filenames) {
+  return new Promise(async (resolve) => {
+    const outputPath = path.join(__dirname, 'output', 'segment.txt');
+    const segmentFileStream = fs.createWriteStream(outputPath, { flags: 'a' });
 
-      for (let i = 0; i < data.length; i++) {
-        for (let j = 0; j < data[i].length; j++) {
-          if (data[i][j] === ' ' || data[i][j] === '\n') {
-            if (word.length) {
-              segmentFile.write(`${word},${i}\n`);
-            }
-            word = '';
-          } else if (/[^а-яА-Я ]+/g.test(data[i][j])) {
-            continue;
-          } else {
-            word += data[i][j].toLowerCase();
-          }
-        }
-      }
 
-      segmentFile.close();
-      setTimeout(() => resolve(), 100);
-    } catch (err) {
-      reject(err);
+    let i = 0;
+    for await ((filename) of filenames) {
+      await processFile(filename, i, segmentFileStream);
+      i++;
     }
+
+    // await Promise.all(filenames.map((filename, i) => processFile(filename, i, segmentFileStream)));
+
+    segmentFileStream.close();
+    segmentFileStream.on('close', () => {
+      console.log('on the disk');
+      resolve();
+    });
   });
+}
+
+async function processFile(filename, fileId, outputStream) {
+  console.log(`Process file: ${filename}`);
+  const data = await read(filename);
+  let word = '';
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] === ' ' || data[i] === '\n') {
+      if (word.length) {
+        outputStream.write(`${word},${fileId}\n`);
+      }
+      word = '';
+    } else if (/[^а-яА-Я ]+/g.test(data[i])) {
+      continue;
+    } else {
+      word += data[i].toLowerCase();
+    }
+  }
 }
 
 function buildOccurrencesMap() {
   const data = fs.readFileSync(path.join(__dirname, 'output', 'segment.txt'), 'utf8');
-  if (!data.length) throw Error('No file "segment.txt"');
+  if (!data.length) throw Error(`No file ${path.join(__dirname, 'output', 'segment.txt')}`);
   const lines = data.split('\n');
+
   const wordDocIdMap = new Map();
   for (let i = 0; i < lines.length; i++) {
     if (!lines[i].length) continue;
@@ -52,6 +64,7 @@ function buildOccurrencesMap() {
       wordDocIdMap.set(word, new Map([[docId, 1]]));
     }
   }
+
   return wordDocIdMap;
 }
 
@@ -80,6 +93,7 @@ function splitArrayByLetters(arr, letters) {
 }
 
 function stepReduce() {
+  console.log(`Step reduce`);
   const wordDocIdMap = buildOccurrencesMap();
   const sortedWordDocIdArr = [...wordDocIdMap.entries()].sort();
   const letters = ['а', 'б', 'ж', 'л', 'п', 'ф'];
