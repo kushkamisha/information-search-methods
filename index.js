0b931b3f46e0ed9ab2de8400cdd87dd2a189b9f7
Misha Kushka <kushka.misha@gmail.com>
2021-02-28 17:06:43 +0200

    Fix making segment files with letters

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const { stepMap, stepReduce } = require('./indexing');
 const filenames = [
   // "cut Война и мир. Том 1.txt",
   // "cut Бесы.txt",
-  "Война и мир. Том 1.txt",
-  "Война и мир. Том 2.txt",
-  "Война и мир. Том 3.txt",
-  "Война и мир. Том 4.txt",
-  "Мастер и Маргарита.txt",
-  "Волшебник Изумрудного города.txt",
-  "Братья Карамазовы.txt",
-  "Идиот.txt",
-  "Униженные и оскорбленные.txt",
-  "Бесы.txt",
+  // "Война и мир. Том 1.txt",
+  // "Война и мир. Том 2.txt",
+  // "Война и мир. Том 3.txt",
+  // "Война и мир. Том 4.txt",
+  // "Мастер и Маргарита.txt",
+  // "Волшебник Изумрудного города.txt",
+  // "Братья Карамазовы.txt",
+  // "Идиот.txt",
+  // "Униженные и оскорбленные.txt",
+  // "Бесы.txt",
 ];
 
 const main = async () => {
   const start = Date.now();
   const data = await Promise.all(filenames.map(filename => read(filename)));
 
-  // stepMap(data);
+  // await stepMap(data);
   stepReduce();
 
   console.log(`Working time is ${Date.now() - start} ms`);
--- indexing.js
+++ indexing.js
@@ -1,30 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-function stepMap(data) {
-  const segmentFile = fs.createWriteStream(path.join(__dirname, 'output', 'segment.txt'), {
-    flags: 'w',
-  });
-  let word = '';
+async function stepMap(data) {
+  return new Promise((resolve, reject) => {
+    try {
+      const segmentFile = fs.createWriteStream(path.join(__dirname, 'output', 'segment.txt'), {
+        flags: 'w',
+      });
+      let word = '';
 
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      if (data[i][j] === ' ' || data[i][j] === '\n') {
-        if (word.length) {
-          segmentFile.write(`${word},${i}\n`);
+      for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+          if (data[i][j] === ' ' || data[i][j] === '\n') {
+            if (word.length) {
+              segmentFile.write(`${word},${i}\n`);
+            }
+            word = '';
+          } else if (/[^а-яА-Я ]+/g.test(data[i][j])) {
+            continue;
+          } else {
+            word += data[i][j].toLowerCase();
+          }
         }
-        word = '';
-      } else if (/[^а-яА-Я ]+/g.test(data[i][j])) {
-        continue;
-      } else {
-        word += data[i][j].toLowerCase();
       }
+
+      segmentFile.close();
+      setTimeout(() => resolve(), 100);
+    } catch (err) {
+      reject(err);
     }
-  }
+  });
 }
 
 function buildOccurrencesMap() {
   const data = fs.readFileSync(path.join(__dirname, 'output', 'segment.txt'), 'utf8');
+  if (!data.length) throw Error('No file "segment.txt"');
   const lines = data.split('\n');
   const wordDocIdMap = new Map();
   for (let i = 0; i < lines.length; i++) {
@@ -46,15 +56,14 @@ function buildOccurrencesMap() {
 }
 
 function splitArrayByLetters(arr, letters) {
-  const ranges = new Array(letters.length + 1).fill();
+  const ranges = new Array(letters.length).fill();
+
   for (let i = 0; i < arr.length - 1; i++) {
     const word = arr[i][0];
     let isPushed = false;
-    // console.log(letters.length);
     for (let j = 0; j < letters.length; j++) {
       const firstLetter = word[0];
-      // console.log({ firstLetter, curr: letters[j], next: letters[j + 1] });
-      if (firstLetter > letters[j]) {
+      if (firstLetter >= letters[j]) {
         if (firstLetter < letters[j + 1] || letters[j + 1] === undefined) {
           if (!ranges[j]) ranges[j] = [];
           ranges[j].push(arr[i]);
@@ -68,22 +77,18 @@ function splitArrayByLetters(arr, letters) {
     }
   }
   return ranges;
-  // first range is at the end => moving it to the front
-  // return [ranges[ranges.length - 1], ...ranges.slice(0, ranges.length - 1)];
 }
 
 function stepReduce() {
   const wordDocIdMap = buildOccurrencesMap();
   const sortedWordDocIdArr = [...wordDocIdMap.entries()].sort();
-  const letters = ['ж', 'л', 'ф'];
+  const letters = ['а', 'б', 'ж', 'л', 'п', 'ф'];
   const ranges = splitArrayByLetters(sortedWordDocIdArr, letters);
-  // const line = ranges[0][0];
-  // console.log(`${line[0]},${[...line[1].entries()].join(',')}`);
-  ranges.forEach((range, i) => {
-    // console.log();
+
+  for (let i = 0; i < ranges.length; i++) {
     fs.writeFileSync(path.join(__dirname, 'output', `segment-${letters[i]}.txt`),
-      range.map(pair => `${pair[0]},${[...pair[1].entries()].join(', ')}`).join('\n'));
-  })
+      ranges[i].map(pair => `${pair[0]},${[...pair[1].entries()].join(', ')}`).join('\n'));
+  }
 }
 
 module.exports = {
