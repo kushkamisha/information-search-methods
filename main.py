180391eb661e700308a10cd93aa6ced7da4f86c4
Misha Kushka <kushka.misha@gmail.com>
2021-03-27 20:32:51 +0200

    Init compression

---
+++ main.py
@@ -0,0 +1,13 @@
+from Compression import Compress
+import os
+
+# directory = 'D:\KMA\\2\IR\\'
+directory = 'data/'
+files = os.listdir(directory)
+books = [directory+x for x in files if x.endswith('.txt')]
+
+if __name__ == "__main__":
+    compr = Compress(books)
+    compr.compress_dict()
+    compr.compress_postings()
+    compr.write()
